e9413b693d6c8b54ac08d17025081efbefefb84d
Connor Nusser <connornusser@gmail.com>
2023-11-22 23:16:21 -0600

    make changes to my app :)

--- components/workoutdetailscreen.js
+++ components/workoutdetailscreen.js
@@ -1,11 +1,23 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Modal, StyleSheet, ScrollView } from 'react-native';
+import { ButtonGroup } from '@rneui/themed'
 
 const WorkoutDetailScreen = ({ route }) => {
   const { workoutNumber, content } = route.params;
-
+  const daysOfWeek = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
+  const [selectedIndex, setSelectedIndex] = useState(0);
   return (
     <View>
+        <View style={styles.container}>
+        <ButtonGroup
+        buttons={daysOfWeek}
+        selectedIndex={selectedIndex}
+        onPress={(value) => {
+            setSelectedIndex(value);
+        }}
+        containerStyle={{ marginBottom: 20 }}
+        />
+      </View>
       <Text>Workout Number: {workoutNumber}</Text>
       <Text>Content: {content}</Text>
       {/* ... Other workout details */}
@@ -13,4 +25,15 @@ const WorkoutDetailScreen = ({ route }) => {
   );
 };
 
+const styles = StyleSheet.create({
+    container: {
+      flexDirection: 'row',
+      justifyContent: 'center', // Center the buttons horizontally
+      flexWrap: 'wrap', // Allow buttons to wrap if the container width is exceeded
+    },
+    button: {
+      margin: 5,
+      width: 40, // Set a fixed width for the buttons
+    },
+  });
 export default WorkoutDetailScreen;
